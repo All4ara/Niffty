9b2f399e75c6c33e264351bd38f2d0d00254a257
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-07-28 19:50:16 -0400

    setup backend api routes for the data

---
+++ backend/data.js
@@ -0,0 +1,67 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: '#86 Becky Cloon Batman',
+            category: 'Rare',
+            image: '/images/Batman1.jpeg',
+            price: 355,
+            countInStock: 10,
+            brand: 'Batman Black & White',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'This is the FIRST EDITION of this character in premium digital format.',
+        },
+        {
+            _id: '2',
+            name: '#79 Jim Lee Nightwing',
+            category: 'Ultra Rare',
+            image: '/images/Nightwing1.jpeg',
+            price: 839,
+            countInStock: 2,
+            brand: 'Batman Black & White',
+            rating: 4.8,
+            numReviews: 11,
+            description: 'This is the FIRST EDITION of the Nightwing character in premium digital format.',
+        },
+        {
+            _id: '3',
+            name: 'Ultraman Seven',
+            category: 'Uncommon',
+            image: '/images/Ultraman1.jpeg',
+            price: 355,
+            countInStock: 20,
+            brand: 'Ultraman',
+            rating: 2.9,
+            numReviews: 5,
+            description: 'Ultraman Suit Version 7, also called Seven Suit, is a newer model made to be worn by ordinary people, who do not have the Ultraman Factor.',
+        },
+        {
+            _id: '4',
+            name: 'Superman',
+            category: 'Uncommon',
+            image: '/images/Superman1.jpeg',
+            price: 550,
+            countInStock: 5,
+            brand: 'DC Superman',
+            rating: 4.8,
+            numReviews: 20,
+            description: 'This is the FIRST EDITION of this character in premium digital format. Max limit: 1 per account',
+        },
+        {
+            _id: '5',
+            name: 'Fat Tony',
+            category: 'Common',
+            image: '/images/FatTony.jpeg',
+            price: 65,
+            countInStock: 0,
+            brand: 'Ron English - Cereal Killers',
+            rating: 3,
+            numReviews: 6,
+            description: 'Introducing Fat Tony: 100% nutrition free, one tonne tiger, with a short shelf life! Fatten up your collection with Ron English Cereal Killer Series.',
+        },
+
+    ]
+}
+
+export default data;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Product from '../components/Product'
 import Rating from '../components/Rating';
 import data from '../data';
 import { Link } from 'react-router-dom'
