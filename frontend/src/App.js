e4484cbf1c04282ca0f854feb506b329bba11138
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-08-17 15:30:33 -0400

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
                 <a className="brand" href="/">Niffty</a>
             </div>
             <div>
-                <a href="/cart">Cartt</a>
+                <a href="/cart">Cart</a>
                 <a href="/signin">Sign In</a>
             </div>
         </header>
