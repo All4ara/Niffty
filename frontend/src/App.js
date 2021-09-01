eb4d7f5a2bcde132035b9bd9e46565ca1309f33a
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-09-01 15:52:48 -0400

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,20 +7,20 @@ import ProductScreen from './screens/ProductScreen.js';
 function App() {
   return (
     <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">Nifffty</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
-        </header>
-        <main>
-          <Route path="/product/:id" component={ProductScreen} ></Route>
-          <Route exact path="/" component={HomeScreen} ></Route>
-        </main>
-        <footer className="row center">All right reserved</footer>
+      <header className="row">
+        <div>
+          <a className="brand" href="/">Nifffty</a>
+        </div>
+        <div>
+          <a href="/cart">Car</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <Route path="/product/:id" component={ProductScreen} ></Route>
+        <Route exact path="/" component={HomeScreen} ></Route>
+      </main>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
